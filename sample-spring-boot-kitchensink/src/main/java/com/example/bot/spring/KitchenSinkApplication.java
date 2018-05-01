d8f235a5461a9c3c7f85408631ac73cee718b534
DEV-KONBU <vps.cafe17@gmail.com>
2018-05-01 23:14:51 +0900

    レスポンスに画像返す

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkApplication.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkApplication.java
@@ -16,20 +16,73 @@
 
 package com.example.bot.spring;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
 @SpringBootApplication
+@LineMessageHandler
 public class KitchenSinkApplication {
+    
+    @Autowired
+    public LineMessagingClient lineMessagingClient;
+
     static Path downloadedContentDir;
 
     public static void main(String[] args) throws IOException {
         downloadedContentDir = Files.createTempDirectory("line-bot");
         SpringApplication.run(KitchenSinkApplication.class, args);
+    }
+
+    @EventMapping
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        final LineMessagingClient client = LineMessagingClient
+                .builder("thLH/MvoqBwCdbBcKOjsSujJNaE2TZV+EAdTRq4e6Gu96R8ogu9Jmt+LaH3Ksgot6E5Hnv+13S97AZVBKpufgmBcVR2sOs14I/tgglaM6PCwkBiHoc3LelXj1r3GAYAhmO76IFXz0rXB9Q7TXPf8bgdB04t89/1O/w1cDnyilFU=")
+                .build();
+
+        final TextMessage textMessage = new TextMessage(event.getMessage().getText());
+
+        // 返信用画像(image)のURL
+        final String originalContentUrl = "https://art4.photozou.jp/pub/784/784/photo/17561757.v1524899011.jpg";
+        final String previewImageUrl = "https://art4.photozou.jp/pub/784/784/photo/17561757_thumbnail.v1524899011.jpg";
+
+        // 返信用画像(image)メッセージ
+        ImageMessage imageMessage = new ImageMessage(originalContentUrl, previewImageUrl);
+
+        final ReplyMessage replyMessage = new ReplyMessage(event.getReplyToken(), Arrays.asList(imageMessage, textMessage));
+
+        final BotApiResponse botApiResponse;
+        try {
+            botApiResponse = client.replyMessage(replyMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println(botApiResponse);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        log.info("handleDefaultMessageEvent = {}", event);
     }
 
 }
